00f4c3a337dacb06929a27decda1dddc0bf621aa
wuxiaobai24 <wuxiaobai24@163.com>
2017-08-26 19:06:18 +0800

    为正方形加上纹理

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "shader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -9,6 +13,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 float xOffset = 0;
+float mix_level = 0;
 void processInput(GLFWwindow *window) {
 	//检查输入（即是否按下某个键) 在Render Loop中执行，为简洁性，独立一个函数
 
@@ -16,8 +21,12 @@ void processInput(GLFWwindow *window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 	if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS) {
-		xOffset += 0.01;
+		xOffset += 0.005;
 		if (xOffset > 1) xOffset = -1;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		mix_level += 0.005;
+		if (mix_level >= 1) mix_level = 0;
 	}
 }
 
@@ -65,33 +74,111 @@ int main() {
 	Shader shader("..\\helloworld\\shader\\vertex.vs",
 		"..\\helloworld\\shader\\fragment.fs");
 
-	//三个顶点
+	//四个顶点
 	float vertices[] = {
-		//位置				//颜色
-		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+		//位置					//颜色				//纹理坐标
+		 0.5f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	1.0f,1.0f,
+		 0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	1.0f,0.0f,
+		 -0.5f, -0.5f, 0.0f,	0.0f, 0.0f,1.0f,	0.0f,0.0f,
+		 -0.5f, 0.5f, 0.0f,		1.0f,1.0f,0.0f,		0.0f,1.0f
+	};
+
+	int indices[] = {
+		0,1,3,
+		1,2,3
 	};
 
 	unsigned int VBO; //对象id
 	glGenBuffers(1, &VBO);//产生一个VBO对象
 	unsigned int VAO;	
 	glGenVertexArrays(1, &VAO);//创建一个VAO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);//绑定VAO
 
-	//把顶点数组复制到缓冲中
+
+						   //把顶点数组复制到缓冲中
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//告知OpenGL如何解释这些顶点数据
 	//参数：顶点属性（位置值，先前在vertexShader源码中设置了location为0了）,
 	//	   顶点属性的大小（vec3),数据的类型，是否标准化，位置数据在缓冲中起始位置的Offset
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	//位置
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	//颜色属性
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	//颜色
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
+	//纹理
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+						   //纹理
+						   //生成纹理
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);//绑定
+
+										  //为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//设置过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_MIRRORED_REPEAT, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	//设置翻转
+	stbi_set_flip_vertically_on_load(true);
+
+	//加载图片
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("..\\container.jpg", &width, &height, &nrChannels,0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		cout << "Failed to load texture" << endl;
+	}
+	//释放图像内存
+	stbi_image_free(data);
+	
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);//绑定
+
+										   //为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	//加载图片
+	
+	data = stbi_load("..\\moonRising.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		cout << "Failed to load texture" << endl;
+	}
+	//释放图像内存
+	stbi_image_free(data);
+
+	shader.use();
+	glUniform1i(glGetUniformLocation(shader.Program, "ourTexture1"), 0);
+	shader.setInt("ourTexture2", 1);
+
+	
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	//渲染循环（Render Loop)
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);//检查输入（按钮）
@@ -101,13 +188,19 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);//清空颜色缓冲
 		
 		//绘制物体
-		shader.use();//激活渲染程序
 		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		//改变uniform
 		shader.setFloat("xOffset", xOffset);
-		//cout << xOffset << endl;
+		shader.setFloat("mix_level", mix_level);
+
+		shader.use();//激活渲染程序
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);//交换颜色缓冲
 		glfwPollEvents();//检查是否有触发事件
--- helloworld/shader/fragment.fs
+++ helloworld/shader/fragment.fs
@@ -1,8 +1,14 @@
 #version 330 core
 out vec4 FragColor;
-in vec3 ourColor;
 
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture1;
+uniform sampler2D ourTexture2;
+uniform float mix_level;
 void main ()
 {
-	FragColor = vec4(ourColor,1.0);
+	FragColor = mix(texture(ourTexture1, vec2(1.0 - TexCoord.x,TexCoord.y)) , texture(ourTexture2, TexCoord),mix_level);
+
 }
